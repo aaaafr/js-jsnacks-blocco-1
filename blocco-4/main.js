502ccc4d24f06f73b8344b2607b2e796cdca0bd4
aaaafr <anastasia.frassini@icloud.com>
2021-05-10 11:38:33 +0200

    object palla

---
+++ blocco-4/main.js
@@ -0,0 +1,12 @@
+// Snack Blocco 4
+// Da mettere in una 
+// sottocartella nella repo della 
+// volta scorsa js-jsnacks-blocco-1 1a:
+// Creare un oggetto palla che abbia le seguenti proprietà.
+// Nome = palla
+// Peso = 10
+
+var palla = {
+    nome : "palla",
+    peso : 10,
+}
